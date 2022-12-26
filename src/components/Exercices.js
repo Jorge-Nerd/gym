1ad095a0c24a96407c897028a6a720329043a189
Jail_IB <jailsonsanches9@gmail.com>
2022-12-26 03:30:10 +0100

    feat:scrooolExercices

--- src/components/Exercices.js
+++ src/components/Exercices.js
@@ -7,16 +7,24 @@ import { exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciceCard from "./ExerciceCard";
 
 const Exercices = ({ exercices, setExercices, bodyPart }) => {
-
-  const exercicesPerPage=9;
+  const exercicesPerPage = 9;
   const [currentPage, setCurrentPage] = useState(1);
 
-  const paginate = (e, value) => {
-    setCurrentPage(value)
+  const indexOfLastExercice = currentPage * exercicesPerPage;
 
-    window.scroll({top:1800, behavior:'smooth'})
+  const indexOfFirstExercice = indexOfLastExercice - exercicesPerPage;
+
+  const currentExercice = exercices.slice(
+    indexOfFirstExercice,
+    indexOfLastExercice
+  );
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scroll({ top: 1800, behavior: "smooth" });
   };
-  
+
   return (
     <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
       <Typography variant="h3" component="h4" mb="46px">
@@ -28,7 +36,7 @@ const Exercices = ({ exercices, setExercices, bodyPart }) => {
         flexWrap="wrap"
         justifyContent="center"
       >
-        {exercices.map((exercice, index) => (
+        {currentExercice.map((exercice, index) => (
           <p>
             <ExerciceCard key={index} exercice={exercice} />
           </p>
